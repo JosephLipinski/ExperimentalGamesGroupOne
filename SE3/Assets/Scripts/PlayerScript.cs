1551fdcb0a73319f385c8a86838539eac30c1580
Sydney Oswald <Scorpia111@windstream.net>
2018-07-09 00:18:24 -0400

    Added Music and Animations for the tall boi

--- SE3/Assets/Scripts/PlayerScript.cs
+++ SE3/Assets/Scripts/PlayerScript.cs
@@ -210,6 +210,7 @@ public class PlayerScript : MonoBehaviour
     {
         transform.position = respawnLocation;
         atSpawn = true;
+        Debug.Log ("die");
     }
 
     private void OnTriggerEnter(Collider other)
